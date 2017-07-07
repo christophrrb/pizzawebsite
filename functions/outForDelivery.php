08fea1f7d1c7fc260db3d33623625b5abc07cfac
christophrrb <christopherballenger@gmail.com>
2017-07-07 19:24:07 -0400

    Changed for different enum value. Formerly "Out For Delivery", now it's "Out for Delivery" (lowercase for).

--- functions/outForDelivery.php
+++ functions/outForDelivery.php
@@ -2,7 +2,7 @@
   require '../connect.php';
   $order_id = $_REQUEST['order_id'];
 
-  $sql = "UPDATE orders SET order_status_cd = 'Out For Delivery' WHERE order_id = $order_id";
+  $sql = "UPDATE orders SET order_status_cd = 'Out for Delivery' WHERE order_id = $order_id";
   $result = $conn->query($sql);
   if ($result) {
     $bool = true;
